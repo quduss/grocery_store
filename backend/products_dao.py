24dff8765ec416d4ce81f86ee8e735d5e6cc3d34
quduss <shuaibqudus33@gmail.com>
2024-05-28 18:51:48 +0100

    Write a function that adds a new product

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -15,6 +15,21 @@ def get_all_products(connection):
              'uom_name': uom_name}
         )
     return response
+
+def insert_new_product(connection, product):
+    cursor = connection.cursor()
+    query = ("INSERT INTO products "
+             "(name, uom_id, price_per_unit) "
+             "VALUES (%s, %s, %s)")
+    data = (product['product_name'], product['uom_id'], product['price_per_unit'])
+    cursor.execute(query, data)
+    connection.commit()
+    return cursor.lastrowid
+
 if __name__ == '__main__':
     connection = get_sql_connection()
-    print(get_all_products(connection))
+    print(insert_new_product(connection, {
+        'product_name': 'cabbage',
+        'uom_id': '1',
+        'price_per_unit': '10'
+    }))

547625307759d1c955dea5060b916cb64600b02e
quduss <shuaibqudus33@gmail.com>
2024-05-29 14:33:31 +0100

    Create route that returns list of all products

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, jsonify
+from products_dao import get_all_products
+from sql_connection import get_sql_connection
 
 app = Flask(__name__)
-
-@app.route('/hello')
-def hello():
-    return "Hello, how are you"
+connection = get_sql_connection()
+@app.route('/getProducts', methods=['GET'])
+def get_products():
+    products = get_all_products(connection)
+    response = jsonify(products)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 if __name__ == '__main__':
     print("Starting Python Flask Server for Grocery Store Management System")

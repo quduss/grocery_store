36a7c7e228de1ee47fe23cc5afee9ae5f2232efa
quduss <shuaibqudus33@gmail.com>
2024-05-30 11:24:07 +0100

    Create routes that deletes a product, retrieve uoms, and inserts a product

--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,41 @@
 from flask import Flask, request, jsonify
-from products_dao import get_all_products
+import products_dao
+import uom_dao
+import json
 from sql_connection import get_sql_connection
 
 app = Flask(__name__)
 connection = get_sql_connection()
 @app.route('/getProducts', methods=['GET'])
 def get_products():
-    products = get_all_products(connection)
+    products = products_dao.get_all_products(connection)
     response = jsonify(products)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/deleteProduct', methods=['POST'])
+def delete_product():
+    return_id = products_dao.delete_product(connection, request.form['product_id'])
+    response = jsonify(
+        {'product_id': return_id}
+    )
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/getUOM', methods=['GET'])
+def get_uom():
+    response = uom_dao.get_uoms(connection)
+    response = jsonify(response)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/insertProduct', methods=['POST'])
+def insert_product():
+    request_payload = json.loads(request.form['data'])
+    product_id = products_dao.insert_new_product(connection, request_payload)
+    response = jsonify({
+        'product_id': product_id
+    })
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 

ba249d7ac50567efb6d576746deadc6b193468d2
quduss <shuaibqudus33@gmail.com>
2024-06-03 16:46:35 +0100

    Create a function that returns list of all orders

--- backend/order_dao.py
+++ backend/order_dao.py
@@ -25,23 +25,24 @@ def insert_order(connection, order):
     cursor.executemany(order_details_query, order_details_data)
     connection.commit()
     return order_id
+def get_all_orders(connection):
+    cursor = connection.cursor()
+    query = ("SELECT * from orders")
+    cursor.execute(query)
+
+    response = []
+    for (order_id, customer_name, total, datetime) in cursor:
+        response.append(
+            {
+                'order_id': order_id,
+                'customer_name': customer_name,
+                'total': total,
+                'datetime': datetime
+            }
+        )
+    return response
 
 
 if __name__ == '__main__':
     connection = get_sql_connection()
-    print(insert_order(connection, {
-        'customer_name': 'paul',
-        'grand_total': '100',
-        'order_details': [
-            {
-                'product_id': 1,
-                'quantity': 2,
-                'total_price': 50
-            },
-            {
-                'product_id': 3,
-                'quantity': 2,
-                'total_price': 40
-            }
-        ]
-    }))
+    print(get_all_orders(connection))
